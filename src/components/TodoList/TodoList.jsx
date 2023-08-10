fe99e81b608df12b742c17bbb73a043961bcc2d1
sbjang <sbjang123456@gmail.com>
2023-08-10 13:31:17 +0900

    feat: add Header for filter

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -2,7 +2,7 @@ import { useCallback, useState } from "react";
 import AddTodo from "../AddTodo/AddTodo";
 import Todo from "../Todo/Todo";
 
-export default function TodoList() {
+export default function TodoList({ filter }) {
   const [todos, setTodos] = useState([
     { id: "123", text: "장보기", status: "active" },
     { id: "124", text: "공부하기", status: "active" },
@@ -19,10 +19,13 @@ export default function TodoList() {
   const handleDelete = useCallback((deleted) => {
     setTodos((prev) => prev.filter((t) => t.id !== deleted.id));
   }, []);
+
+  const filtered = getFilteredItems(todos, filter);
+
   return (
     <section>
       <ul>
-        {todos.map((item) => (
+        {filtered.map((item) => (
           <Todo
             key={item.id}
             todo={item}
@@ -34,4 +37,12 @@ export default function TodoList() {
       <AddTodo onAdd={handleAdd} />
     </section>
   );
+}
+
+function getFilteredItems(todos, filter) {
+  if (filter === "all") {
+    return todos;
+  }
+
+  return todos.filter((todo) => todo.status === filter);
 }
